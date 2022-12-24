f0aad98a18492f923d479ffac8189f4fc3d9de07
f-dagher <fdagher95@gmail.com>
2022-12-24 02:08:33 -0500

    4.8 blog list tests step 1

---
+++ part4/bloglist/tests/blog_api.test.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+
+const api = supertest(app)
+
+const Blog = require('../models/blog')
+const helper = require('./api_test_helper')
+
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(helper.initialBlogs)
+}, 100000)
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+}, 100000)
+
+test('all blogs are returned', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body).toHaveLength(helper.initialBlogs.length)
+}, 100000)
+
+afterAll(() => {
+  mongoose.connection.close()
+})
