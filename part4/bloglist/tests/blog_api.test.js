d6bf2346de73305d54fe9bab08037f5dc6de9bf2
f-dagher <fdagher95@gmail.com>
2022-12-27 03:04:55 -0500

    4.12 blog list tests step 5

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -40,7 +40,7 @@ describe('adding blogs and ids', () => {
 })
 
 describe('Posting new blogs' , () => {
-  test('a valid blog can be added ', async () => {
+  test('that is valid is added', async () => {
     const newBlog = {
       title: 'A new blog',
       author: 'New Person',
@@ -62,8 +62,57 @@ describe('Posting new blogs' , () => {
       'A new blog'
     )
   })
-})
+  test('with missing likes defaults to 0 ', async () => {
+    const newBlog = {
+      title: 'I didnt specify likes',
+      author: 'Me',
+      url: 'example.com'
+    }
+  
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+  
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd[blogsAtEnd.length - 1].likes).toBe(0)
+  })
 
+  test('with missing title results in bad request', async () => {
+    const newBlog = {
+      author: 'Me',
+      url: 'example.com',
+      likes: 4
+    }
+  
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+
+    const blogsAtEnd = await helper.blogsInDb()
+
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+  }, 100000)
+
+  test('with missing url results in bad request', async () => {
+    const newBlog = {
+      title: 'Missing an url',
+      author: 'Me',
+      likes: 4
+    }
+  
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+
+    const blogsAtEnd = await helper.blogsInDb()
+
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+  }, 100000)
+})
 
 afterAll(() => {
   mongoose.connection.close()
