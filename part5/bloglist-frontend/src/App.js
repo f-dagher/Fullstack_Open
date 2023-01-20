88e2a0188b0592b135e7c961a8c6b697320e5480
f-dagher <fdagher95@gmail.com>
2023-01-20 02:27:24 -0500

    5.1 bloglist frontend step 1

---
+++ part5/bloglist-frontend/src/App.js
@@ -0,0 +1,92 @@
+import { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import blogService from './services/blogs'
+import Notification from './components/Notification'
+import loginService from './services/login'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [username, setUsername] = useState('') 
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    blogService.getAll().then(blogs =>
+      setBlogs( blogs )
+    )  
+  }, [])
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+      setUser(user)
+      blogService.setToken(user.token)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage('wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
+  const blogsToShow = showAll
+    ? blogs
+    : blogs.filter(blog => blog.title)
+
+    const loginForm = () => (
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+            <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+            <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>      
+    )
+
+    return (
+      <div>
+        <h1>Blogs</h1>
+        <Notification message={errorMessage} />
+  
+        {user === null ?
+          loginForm() :
+          <div>
+            <p>{user.name} logged in</p>
+            {blogsToShow.map(blog => 
+              <Blog
+                key={blog.id}
+                blog={blog}
+              />
+            )}
+          </div>
+        }
+      </div>
+    )
+}
+
+export default App
+
+/*
+       
+*/
