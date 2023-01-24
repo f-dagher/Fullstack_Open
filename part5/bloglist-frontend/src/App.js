5907611f1c4b87909a5096b00c4d9d115a864fcf
f-dagher <fdagher95@gmail.com>
2023-01-24 02:17:52 -0500

    5.3 bloglist frontend step 3

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -6,6 +6,7 @@ import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [newBlog, setNewBlog] = useState({ title: '', author: '', url: ''})
   const [showAll, setShowAll] = useState(true)
   const [errorMessage, setErrorMessage] = useState(null)
   const [username, setUsername] = useState('') 
@@ -18,6 +19,15 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
   const handleLogin = async (event) => {
     event.preventDefault()
     try {
@@ -26,6 +36,9 @@ const App = () => {
       })
       setUser(user)
       blogService.setToken(user.token)
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      ) 
       setUsername('')
       setPassword('')
     } catch (exception) {
@@ -36,10 +49,46 @@ const App = () => {
     }
   }
 
+  const handleLogout = () => {
+    window.localStorage.clear()
+    setUser(null)
+  }
+
+  const addBlog = (event) => {
+    event.preventDefault()
+    const blogObject = {
+      title: newBlog.title,
+      author: newBlog.author,
+      url: newBlog.url,
+    }
+
+    blogService
+      .create(blogObject)
+      .then(returnedBlog => {
+        setBlogs(blogs.concat(returnedBlog))
+        setNewBlog({ title: '', author: '', url: ''})
+      })
+  }
+
+  
+  const handleTitleChange = (event) => {
+    setNewBlog({...newBlog, title: event.target.value})
+    console.log(newBlog)
+  }
+  const handleAuthorChange = (event) => {
+    setNewBlog({...newBlog, author: event.target.value})
+    console.log(newBlog)
+  }
+  const handleUrlChange = (event) => {
+    setNewBlog({...newBlog, url: event.target.value})
+    console.log(newBlog)
+  }
+
+
   const blogsToShow = showAll
     ? blogs
     : blogs.filter(blog => blog.title)
-
+  
     const loginForm = () => (
       <form onSubmit={handleLogin}>
         <div>
@@ -64,15 +113,54 @@ const App = () => {
       </form>      
     )
 
+    const blogForm = () => (
+      <div>
+        <h2> Create a new blog </h2>
+        <form onSubmit={addBlog}>
+          <div>
+            Title: 
+              <input
+              type="text"
+              value={newBlog.title}
+              name="Title"
+              onChange={handleTitleChange}
+            />
+          </div>
+          <div>
+            Author: 
+              <input
+              type="text"
+              value={newBlog.author}
+              name="Author"
+              onChange={handleAuthorChange}
+            />
+          </div>
+          <div>
+            URL: 
+              <input
+              type="text"
+              value={newBlog.url}
+              name="Url"
+              onChange={handleUrlChange}
+            />
+          </div>
+          <button type="submit">save</button>
+        </form>
+      </div>
+    )
+
     return (
       <div>
         <h1>Blogs</h1>
         <Notification message={errorMessage} />
-  
         {user === null ?
           loginForm() :
           <div>
-            <p>{user.name} logged in</p>
+            <p>
+              {user.name} logged in 
+              <button onClick={handleLogout}>logout</button>
+            </p>
+            {blogForm()}
             {blogsToShow.map(blog => 
               <Blog
                 key={blog.id}
@@ -88,5 +176,5 @@ const App = () => {
 export default App
 
 /*
-       
+Used for testing  
 */
